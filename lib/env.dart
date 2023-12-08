3089af2f51790df713307d732b49e0fdca995c36
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-12-08 02:27:28 +0200

    some work

--- lib/env.dart
+++ lib/env.dart
@@ -1,8 +1,7 @@
 import 'dart:ui';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-const String mainUrl = 'http://192.168.1.8/almajhoud/public/api/v1';
+const String mainUrl = 'http://192.168.1.8/api/v1';
 SharedPreferences? sharedPreferences;
 dynamic userInfo = sharedPreferences!.getString('user');
 Map? sessionUser;
